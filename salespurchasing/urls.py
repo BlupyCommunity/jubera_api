c8197711c18aff1b3c94847c98740607d93bcd84
berackin <berackin@gmail.com>
2019-07-07 22:09:47 +0700

    Menambahkan resource API UserUpdate (mengubah user yang ada)

--- salespurchasing/urls.py
+++ salespurchasing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from salespurchasing.views.users import UserLogin, UserNew
+from salespurchasing.views.users import UserLogin, UserNew, UserUpdate
 
 app_name = 'api'
 
 urlpatterns = [
     path('UserLogin/', UserLogin.as_view(), name='UserLogin'),
     path('UserNew/', UserNew.as_view(), name='UserNew'),
+    path('UserUpdate/', UserUpdate.as_view(), name='UserUpdate'),
 ]
--- salespurchasing/views/users.py
+++ salespurchasing/views/users.py
@@ -68,6 +68,10 @@ class UserNew(APIView):
         if users:
             raise ParseError('Username sudah ada')
 
+        users = User.objects.filter(email=data.get('email'))
+        if users:
+            raise ParseError('Email sudah ada')
+
     @transaction.atomic()
     def execute(self, request):
         data = request.data
@@ -107,11 +111,99 @@ class UserNew(APIView):
 
 
 class UserUpdate(APIView):
-    pass
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def check_pass(self, request):
+        data = request.data
+
+        if not data.get('username'):
+            raise ParseError('Username tidak boleh kosong')
+
+        if not data.get('password'):
+            raise ParseError('Password tidak boleh kosong')
+
+        if not data.get('first_name'):
+            raise ParseError('Nama depan tidak boleh kosong')
+
+        if not data.get('last_name'):
+            raise ParseError('Nama belakang tidak boleh kosong')
+
+        if data.get('is_active') is None:
+            raise ParseError('Aktivasi tidak valid')
+
+        if not isinstance(data.get('is_active'), bool):
+            raise ParseError('Aktivasi tidak valid')
+
+        if not data.get('email'):
+            raise ParseError('Email tidak boleh kosong')
+
+        if data.get('is_superuser') is None:
+            raise ParseError('Hak akses tidak valid')
+
+        if not isinstance(data.get('is_superuser'), bool):
+            raise ParseError('Hak akses tidak valid')
+
+        users = User.objects.filter(username=data.get('username'))
+        if not users:
+            raise ParseError('Username tidak terdaftar')
+
+        user = User.objects.get(username=data.get('username'))
+        emails = User.objects.filter(email=data.get('email')).exclude(username=user.username)
+
+        if emails:
+            raise ParseError('Email telah terdaftar')
+
+        if not data.get('username_new'):
+            raise ParseError('Username yang baru tidak valid')
+
+        usernames = User.objects.filter(username=data.get('username_new')).exclude(username=user.username)
+        if usernames:
+            raise ParseError('Username telah terdaftar sebelumnya')
 
 
-class UserDisabled(APIView):
-    pass
+    @transaction.atomic()
+    def execute(self, request):
+        data = request.data
+
+        user = User.objects.get(username=data.get('username'))
+
+        if user.first_name != data.get('first_name'):
+            user.first_name = data.get('first_name')
+
+        if user.last_name != data.get('last_name'):
+            user.last_name = data.get('last_name')
+
+        if user.email != data.get('email'):
+            user.email = data.get('email')
+
+        if user.is_superuser != data.get('is_superuser'):
+            user.is_superuser = data.get('is_superuser')
+
+        if not user.check_password(data.get('password')):
+            user.set_password(data.get('password'))
+
+        if user.is_superuser != data.get('is_superuser'):
+            user.is_superuser = data.get('is_superuser')
+
+        if user.is_active != data.get('is_active'):
+            user.is_active = data.get('is_active')
+
+        if user.username != data.get('username_new'):
+            user.username = data.get('username_new')
+
+        user.save()
+
+        Token.objects.get(user=user).delete()
+        Token.objects.create(user=user)
+
+        return user
+
+    def post(self, request):
+        self.check_pass(request)
+        user = self.execute(request)
+
+        return Response(UserSerializer(user, many=False).data)
 
 
 class UserList(generics.ListAPIView):
